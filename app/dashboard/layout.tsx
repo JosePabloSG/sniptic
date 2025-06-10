62598d429c32602fcd8c72d983087af21bcdb0f1
JosePabloSG <suarezgomezjosepablo03@gmail.com>
2025-06-09 19:26:10 -0600

    refactor: clean up formatting in DashboardLayout component

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,3 @@
-
 import { AppSidebar } from "@/components/navigation/side-bar/app-sidebar"
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
 
@@ -9,19 +8,19 @@ export default function DashboardLayout({
 }) {
   return (
     <SidebarProvider
-    style={
-      {
-        "--sidebar-width": "calc(var(--spacing) * 72)",
-        "--header-height": "calc(var(--spacing) * 12)",
-      } as React.CSSProperties
-    }
-  >
-    <AppSidebar variant="inset" />
-    <SidebarInset>
-      <main className="flex-1 overflow-auto">
-      {children}
-    </main>
-    </SidebarInset>
-  </SidebarProvider>
+      style={
+        {
+          "--sidebar-width": "calc(var(--spacing) * 72)",
+          "--header-height": "calc(var(--spacing) * 12)",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <main className="flex-1 overflow-auto">
+          {children}
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
   )
-} 
+}
